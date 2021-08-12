cfd229873fa4ca634a07d4553d17c3bc5eb690fd
bitcamp <bitcamp@DESKTOP-J4V03M8>
2021-08-12 19:53:24 +0900

    commit

--- src/ch06_1_oop/Korean.java
+++ src/ch06_1_oop/Korean.java
@@ -1,11 +1,17 @@
 package ch06_1_oop;
 
 public class Korean {
-// 필드
+//필드
 	String nation = "대한민국";
 	String name;
 	String ssn;
 
-	// 생성자
-	public korea(String n, String )
+//생성자
+	public Korean(String n, String s) {
+
+		name = n;
+		ssn = s;
+
+	}
+
 }
--- src/ch06_1_oop/Student.java
+++ src/ch06_1_oop/Student.java
@@ -1,8 +1,5 @@
 package ch06_1_oop;
 
 public class Student {
-	public static void main(String[] args) {
-		int num;
-		
-	}
+
 }
