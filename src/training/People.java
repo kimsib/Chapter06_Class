7f988e8f3c1bdacf3646fdc029ef33c1706b42b9
sibeomkim <sibeomkim@LAPTOP-5SBHBR47>
2021-08-20 09:08:21 +0900

    commit

---
+++ src/training/People.java
@@ -0,0 +1,19 @@
+package training;
+
+public class People {
+
+		public int age;
+		public String name;
+		public boolean isMarried;
+		public int children;
+		
+		public void peopleinfo() {
+			
+			System.out.println("나이 : " + age);
+			System.out.println("이름: " + name);
+			System.out.println("결혼 여부 : " + isMarried);
+			System.out.println("자녀 수 : " + children);
+		}
+	
+	
+}
---
+++ src/training/PeopleTest.java
@@ -0,0 +1,25 @@
+package training;
+
+public class PeopleTest {
+	public static void main(String[] args) {
+		
+		People peopleJames = new People();
+		
+		peopleJames.age = 40;
+		peopleJames.name = "james";
+		peopleJames.isMarried = true;
+		peopleJames.children = 3;
+		
+		peopleJames.peopleinfo();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
